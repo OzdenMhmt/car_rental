c5c89b1e86063ede260b208aa7398eeaa6283285
Mehmet <m.sozdens@gmail.com>
2021-12-29 21:45:41 +0300

    day10

--- src/main/java/com/lecture/car_rental/CarRentalApplication.java
+++ src/main/java/com/lecture/car_rental/CarRentalApplication.java
@@ -2,6 +2,10 @@ package com.lecture.car_rental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class CarRentalApplication {
@@ -10,4 +14,23 @@ public class CarRentalApplication {
         SpringApplication.run(CarRentalApplication.class, args);
     }
 
+    //cors ayarlari
+
+
+    public FilterRegistrationBean<CorsFilter>corsFilter(){
+        FilterRegistrationBean<CorsFilter> registrationBean =new FilterRegistrationBean<>();
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config =new CorsConfiguration();
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        source.registerCorsConfiguration("/**",config);
+        registrationBean.setFilter(new CorsFilter(source));
+        registrationBean.setOrder(0);
+        return registrationBean;
+    }
 }
